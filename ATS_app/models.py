3174cef7a334ee08f0016f29d2e5135836acd761
gloribeth <gnavarrosickles@student.fullsail.edu>
2023-08-15 12:49:15 -0400

    gs

--- ATS_app/models.py
+++ ATS_app/models.py
@@ -1,34 +1,51 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# User Model 
+# =========================
+# Custom User Model
+# =========================
 class CustomUser(AbstractUser):
+    RECRUITER = 'recruiter'
+    CANDIDATE = 'candidate'
+    
     USER_TYPE_CHOICES = (
-        (1, 'recruiter'),
-        (2, 'candidate'),
+        (RECRUITER, 'Recruiter'),
+        (CANDIDATE, 'Candidate'),
     )
-    user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES)
+    
+    user_type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
 
-# Job Posting, Candidate and Application Models 
+# =========================
+# Job Related Models
+# =========================
 class JobPost(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    created_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True)  # Optional: to tie job post to a recruiter
+    created_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True, related_name='job_posts')  
 
 class Candidate(models.Model):
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)  # Link candidate to user for authentication and profile
-    name = models.CharField(max_length=100)
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='candidate_profile')  # Link candidate to user
     contact_info = models.TextField()
     resume = models.FileField(upload_to='resumes/')
 
 class Application(models.Model):
+    PENDING = 'pending'
+    ACCEPTED = 'accepted'
+    REJECTED = 'rejected'
+    
     APPLICATION_STATUS_CHOICES = (
-        ('pending', 'Pending'),
-        ('accepted', 'Accepted'),
-        ('rejected', 'Rejected'),
+        (PENDING, 'Pending'),
+        (ACCEPTED, 'Accepted'),
+        (REJECTED, 'Rejected'),
     )
-    candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
-    job_post = models.ForeignKey(JobPost, on_delete=models.CASCADE)
-    application_date = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=10, choices=APPLICATION_STATUS_CHOICES, default='pending')
 
+    candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE, related_name='applications')
+    job_post = models.ForeignKey(JobPost, on_delete=models.CASCADE, related_name='applications')
+    application_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=10, choices=APPLICATION_STATUS_CHOICES, default=PENDING)
+
+#References: 
+#https://docs.djangoproject.com/en/3.2/topics/db/models/
+#https://docs.djangoproject.com/en/3.2/topics/auth/default/
+#https://docs.djangoproject.com/en/3.2/topics/http/file-uploads/ 
--- ATS_app/views.py
+++ ATS_app/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm 
-from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required 
 from django.views.decorators.http import require_POST, require_GET
-from .models import JobPost, Application
+from .models import JobPost, Application, Candidate, CustomUser
 from .forms import CustomUserCreationForm
 from .decorators import recruiter_required, candidate_required
 from django.contrib import messages
 from django.urls import reverse
 from django.contrib.auth.views import LoginView
+from django.shortcuts import redirect
 
 
 class CustomLoginView(LoginView):
+    """Handles custom redirection after login based on user type."""
 
     def get_success_url(self):
         if self.request.user.user_type == 1:  # Recruiter
@@ -19,40 +20,70 @@ class CustomLoginView(LoginView):
         elif self.request.user.user_type == 2:  # Candidate
             return reverse('candidate_dashboard')
         else:
-            return reverse('home')  # default redirect if something goes wrong
+             return reverse('home')  # default redirect if something goes wrong
+
+def user_dashboard(request):
+    # Check if user is authenticated
+    if not request.user.is_authenticated:
+        return redirect('registration/login.html')  # Redirect to login page or view name
+
+    # Check user type and redirect accordingly
+    if request.user.user_type == 'RECRUITER':
+        return redirect('recruiter_dashboard.html')  # Redirect to the recruiter's dashboard
+
+    elif request.user.user_type == 'CANDIDATE':
+        return redirect('candidate_dashboard.html')  # Redirect to the candidate's dashboard
+
+    # Handle other user types or a default case
+    else:
+        return redirect('ats_app/user_dashboard.html')  # Redirect to a default dashboard or error page
+
 
 
 @login_required
 @candidate_required
 def candidate_dashboard(request):
-    applications = Application.objects.filter(candidate__user=request.user)
+    """Displays the dashboard for candidates, showing their applications."""
+    
+    candidate = Candidate.objects.get(user=request.user)
+    applications = Application.objects.filter(candidate=candidate)
     context = {'applications': applications}
-    return render(request, 'candidate_dashboard.html', context)
+    return render(request, 'Templates/candidate_dashboard.html', context)
 
 
 @login_required
 @recruiter_required
 def recruiter_dashboard(request):
+    """Displays the dashboard for recruiters, showing their job postings."""
+    
     job_listings = JobPost.objects.filter(created_by=request.user)
     context = {'job_listings': job_listings}
-    return render(request, 'recruiter_dashboard.html', context)
+    return render(request, 'Templates/recruiter_dashboard.html', context)
 
 
 def home(request):
+    """The main homepage view."""
+    
     return render(request, 'base.html')
 
 
 def job_listings(request):
+    """Displays all available job listings."""
+    
     jobs = JobPost.objects.all()
     return render(request, 'ats_app/job_listing.html', {'jobs': jobs})
 
 
 @require_POST
 @login_required
+@candidate_required
 def apply_for_job(request, job_id):
+    """Allows a candidate to apply for a job."""
+    
     job = get_object_or_404(JobPost, pk=job_id)
-    if not Application.objects.filter(candidate=request.user, job_post=job).exists():
-        Application.objects.create(candidate=request.user, job_post=job)
+    candidate = Candidate.objects.get(user=request.user)
+    if not Application.objects.filter(candidate=candidate, job_post=job).exists():
+        Application.objects.create(candidate=candidate, job_post=job)
         messages.success(request, 'Successfully applied for the job!')
     else:
         messages.warning(request, 'You have already applied for this job.')
@@ -60,11 +91,15 @@ def apply_for_job(request, job_id):
 
 
 def job_detail(request, job_id):
+    """Displays detailed information about a specific job."""
+    
     job = get_object_or_404(JobPost, id=job_id)
     return render(request, 'ats_app/job_detail.html', {'job': job})
 
 
 def signup_view(request):
+    """Handles user signup."""
+    
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
@@ -79,6 +114,8 @@ def signup_view(request):
 @recruiter_required
 @require_POST
 def add_job_post(request):
+    """Allows recruiters to post new job listings."""
+    
     title = request.POST.get('title')
     description = request.POST.get('description')
     job_post = JobPost(title=title, description=description, created_by=request.user)
@@ -90,6 +127,8 @@ def add_job_post(request):
 @login_required
 @recruiter_required
 def review_applications(request, job_id):
+    """Allows recruiters to review applications for a specific job."""
+    
     job = get_object_or_404(JobPost, id=job_id, created_by=request.user) 
     applications = Application.objects.filter(job_post=job)
     context = {'job': job, 'applications': applications}
@@ -100,6 +139,8 @@ def review_applications(request, job_id):
 @login_required
 @recruiter_required
 def update_application_status(request, application_id):
+    """Allows recruiters to update the status of an application."""
+    
     application = get_object_or_404(Application, id=application_id)
     
     if application.job_post.created_by != request.user:
@@ -115,3 +156,10 @@ def update_application_status(request, application_id):
         messages.error(request, 'Invalid application status.')
 
     return redirect(reverse('review_applications', args=[application.job_post.id]))
+
+
+
+#References:
+#https://www.w3schools.com/django/django_views.php 
+#https://realpython.com/tutorials/django/
+#https://tutorial.djangogirls.org/en/ 
