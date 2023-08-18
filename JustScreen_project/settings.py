bd3173d605f822cc0a43f74bbc66d724e761701f
Glori <glori.sickles@gmail.com>
2023-08-17 20:31:59 -0400

    Push to public profile

--- JustScreen_project/settings.py
+++ JustScreen_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-a^%0+^uian_6if!=yna)d(=u@0xf6%3i#x+s3m#t_kp*j$&qbs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.pythonanywhere.com']
 
 
 # Application definition
